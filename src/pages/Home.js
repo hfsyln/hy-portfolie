6a029a58644e18292d16a3431b4f6ec1f3b23371
hafsa <hfsyln93@gmail.com>
2023-02-27 16:20:30 +0300

    edit home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Pedro</h2>
+        <h2> Hi, My Name is Hafsa</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
           <LinkedInIcon />
@@ -26,16 +26,16 @@ function Home() {
               Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
             </span>
           </li>
-          <li className="item">
+          {/* <li className="item">
             <h2>Back-End</h2>
             <span>
               NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
               MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
             </span>
-          </li>
+          </li> */}
           <li className="item">
             <h2>Languages</h2>
-            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+            <span>JavaScript, Python, TypeScript</span>
           </li>
         </ol>
       </div>
