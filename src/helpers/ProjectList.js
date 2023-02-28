be11dea4290cd57327cd6e14f6dd23779a1927db
hafsa <hfsyln93@gmail.com>
2023-03-01 01:47:33 +0300

    edit

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,8 +2,8 @@ import Proj1 from "../assest/image1.png";
 import Proj2 from "../assest/image2.png";
 import Proj3 from "../assest/image3.png";
 import Proj4 from "../assest/image4.png";
-// import Proj5 from "../assets/proj5.jpg";
-// import Proj6 from "../assets/proj6.png";
+import Proj5 from "../assest/image5.png";
+import Proj6 from "../assest/image6.png";
 
 export const ProjectList = [
   {
@@ -31,13 +31,15 @@ export const ProjectList = [
     link: "https://github.com/hfsyln/react-tracker"
   },
   {
-    name: "Dashboard Visualizer",
-   // image: Proj5,
-    skills: "JavaScript,HTML,CSS",
+    name: "Employees List",
+    image: Proj5,
+    skills: "React.js, HTML, CSS",
+    link: "https://github.com/hfsyln/react-employee"
   },
   {
-    name: "Mobile Game",
-   // image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
+    name: "Languages-Software",
+    image: Proj6,
+    skills: "React.js, HTML. CSS, Bootstrap",
+    link: "https://github.com/hfsyln/react-languages"
   },
 ];
